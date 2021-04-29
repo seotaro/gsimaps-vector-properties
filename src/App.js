b74aedb01c2764190a6d02be52b9891bf3aaf6d1
Taro Seo <mail2017@seotaro.com>
2021-04-29 14:21:13 +0900

    add attribute

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { GeoJsonLayer } from '@deck.gl/layers';
 import { MapView } from '@deck.gl/core';
 import { MVTLayer } from '@deck.gl/geo-layers';
 import './App.css';
+import { Fragment } from 'react';
 
 const INITIAL_VIEW_STATE = {
   longitude: 138.0,
@@ -49,62 +50,71 @@ function App() {
   };
 
   return (
-    <DeckGL
-      initialViewState={INITIAL_VIEW_STATE}
-      controller={true}
-      getTooltip={({ object }) => tooltip(object)}
-      getCursor={({ isHovering }) => isHovering ? 'pointer' : 'grab'}
-    >
-      <MVTLayer
-        data={`https://cyberjapandata.gsi.go.jp/xyz/experimental_bvmap/{z}/{x}/{y}.pbf`}
-        minZoom={4}
-        maxZoom={16}
+    <Fragment>
+      <DeckGL
+        initialViewState={INITIAL_VIEW_STATE}
+        controller={true}
+        getTooltip={({ object }) => tooltip(object)}
+        getCursor={({ isHovering }) => isHovering ? 'pointer' : 'grab'}
+      >
+        <MVTLayer
+          data={`https://cyberjapandata.gsi.go.jp/xyz/experimental_bvmap/{z}/{x}/{y}.pbf`}
+          minZoom={4}
+          maxZoom={16}
 
-        // tooltip を表示するにはサブレイヤーの pickable を有効にする。
-        renderSubLayers={props => {
-          const {
-            bbox: { west, south, east, north }
-          } = props.tile;
+          // tooltip を表示するにはサブレイヤーの pickable を有効にする。
+          renderSubLayers={props => {
+            const {
+              bbox: { west, south, east, north }
+            } = props.tile;
 
-          return new GeoJsonLayer(props, {
-            data: props.data,
-            bounds: [west, south, east, north],
-            pickable: true,
-            highlightColor: [255, 127, 127],
-            autoHighlight: true,
-          });
-        }}
+            return new GeoJsonLayer(props, {
+              data: props.data,
+              bounds: [west, south, east, north],
+              pickable: true,
+              highlightColor: [255, 127, 127],
+              autoHighlight: true,
+            });
+          }}
 
-        getFillColor={f => {
-          switch (f.geometry.type) {
-            case 'Point':
-              return [96, 96, 96];
-            default:
-              return [192, 192, 192];
-          }
-        }}
+          getFillColor={f => {
+            switch (f.geometry.type) {
+              case 'Point':
+                return [96, 96, 96];
+              default:
+                return [192, 192, 192];
+            }
+          }}
 
-        getLineColor={f => {
-          switch (f.geometry.type) {
-            case 'Point':
-            case 'Polygon':
-            case 'MultiPolygon':
-              return [128, 128, 128, 0];
-            default:
-              return [128, 128, 128];
-          }
-        }}
+          getLineColor={f => {
+            switch (f.geometry.type) {
+              case 'Point':
+              case 'Polygon':
+              case 'MultiPolygon':
+                return [128, 128, 128, 0];
+              default:
+                return [128, 128, 128];
+            }
+          }}
 
-        lineWidthMinPixels={1}
+          lineWidthMinPixels={1}
 
-        pointRadiusScale={1}
-        pointRadiusUnits={'pixels'}
-        pointRadiusMinPixels={4}
-      />
+          pointRadiusScale={1}
+          pointRadiusUnits={'pixels'}
+          pointRadiusMinPixels={4}
+        />
 
-      <MapView id="map" controller={true} repeat >
-      </MapView>
-    </DeckGL>
+        <MapView id="map" controller={true} repeat >
+        </MapView>
+      </DeckGL>
+      <div className="attribution">
+        <a href="https://maps.gsi.go.jp/development/vt_expt.html"
+          target="_blank"
+          rel="noopener noreferrer" >
+          © 国土地理院ベクトルタイル提供実験
+        </a>
+      </div>
+    </Fragment>
   );
 }
 
